993bee189c09580b29ffe7f2c66fac406f903fdc
Pranav Mamidanna <pranav.mamidanna@gmail.com>
2015-03-31 20:22:59 +0530

    FitzHugh-Nagumo PPA

--- FitzHugh-Nagumo.m
+++ FitzHugh-Nagumo.m
@@ -4,13 +4,17 @@
 % 
 % Description:
 % Fitzhugh-Nagumo Model Simulation
-% 1. Bifurcation Diagram:
-% 2. Phase Plane Portrait:
-%
-%
-%
-%
-%
+% This 2-D model imitates spike generation by Hodgkin-Huxley type 
+% "neurons". The equations are given by:
+%       V' = V − (V^3)/3 − W + I
+%       W' = 0.08(V + 0.7 − 0.8W)
+%       Where,
+%       V = Membrane Potential.
+%       W = Recovery Variable (Capture Gating dynamics).
+%       I = Injected Current.
+% This code generates the Phase Plane Portrait of the system and 
+% the Bifurcation Diagram as the injected stimulus current is 
+% varied. 
 %
 % Author:
 % Pranav Mamidanna, December 2014
@@ -32,6 +36,8 @@ f = @(w,v) [ v(1) - v(1).^3/3 - v(2) + i;
              0.7*(v(1) + 0.7 - 0.8*v(2)) ];
 g = @(v) f(0,v);
 
+% Finding the equilibria of the system at every "I" value.
+% Deny fsolve ruining the command window.
 options = optimset('Display','off');
 fixedPoints = fsolve(g,[0 0], options);
 Vs = fixedPoints(1);
@@ -41,8 +47,8 @@ Ws = fixedPoints(2);
 % Eigenvalues and Coloring Scheme.
 J = [ [1 - Vs^2, -1]; [0.7, -0.8*0.7]];
 Lambda = eig(J);
-if real(Lambda(1)) > 0; color = [1 0 0];
-else color = [0 0 1];
+if real(Lambda(1)) > 0; color = 'r.';
+else color = 'b.';
 end;
 
 % Determining Nullclines. (dv/dt = 0, dw/dt = 0)
@@ -69,10 +75,10 @@ axis tight;
 subplot(1,2,2);
 hold on;
 [W, V] = ode45(f, [0 100], V(length(W),:) );
-[W, V] = ode45(f, [0 100], V(length(W),:) );
+%[W, V] = ode45(f, [0 100], V(length(W),:) );
 plot(i, min(V(:,1)), 'm.');
 plot(i, max(V(:,1)), 'm.');
-plot(i, Vs,'.','color',color);
+plot(i, Vs,color);
 title('Bifurcation Diagram');
 xlabel('I'); ylabel('V_e_q');
 axis tight;
